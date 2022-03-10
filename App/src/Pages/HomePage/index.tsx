7ec99f0b284ff046fc2c22531dafe2500d69094a
bin <bin.zhou@datareachable.com>
2022-03-10 14:29:04 +0800

    🎨 test 2022/03/10

--- App/src/Pages/HomePage/index.tsx
+++ App/src/Pages/HomePage/index.tsx
@@ -15,23 +15,6 @@
 /** This section will include all the necessary dependence for this tsx file */
 import { Alert, Icon } from '@datareachable/dr_front_componentlibrary';
 import React from 'react';
-import ABTestingCard from './Components/ABTestingCard';
-import AddArgument from './Components/AddArgument';
-import AddNewPaper from './Components/AddNewPaper';
-import ChoosePaper from './Components/ChoosePaper';
-import EditPaper from './Components/EditPaper';
-import EditReturnValue from './Components/EditReturnValue';
-import HoverContent from './Components/HoverContent';
-import NoPermissionBox from './Components/NoPermissionBox';
-import PluginDetails from './Components/PluginDetails';
-import PluginDropDown from './Components/PluginDropDown';
-import PluginsCard from './Components/PluginsCard';
-import ResetPaper from './Components/ResetPaper';
-import SearchPluginInput from './Components/SearchPluginInput';
-import TabPane from './Components/TabPane';
-import Tabs from './Components/Tabs';
-import { TextEditor } from './Components/TextEditor';
-import ToolTip from './Components/ToolTip';
 import style from './style.scss';
 /* <------------------------------------ **** DEPENDENCE IMPORT END **** ------------------------------------ */
 /* <------------------------------------ **** INTERFACE START **** ------------------------------------ */
@@ -48,56 +31,7 @@ const Home = (): JSX.Element => {
     /* <------------------------------------ **** FUNCTION START **** ------------------------------------ */
     /************* This section will include this component general function *************/
     /* <------------------------------------ **** FUNCTION END **** ------------------------------------ */
-    return (
-        <div style={{ marginTop: '200px', marginLeft: '200px' }}>
-            {/* 弹窗 */}
-            <Alert status={true} custom={true} width="auto" height="auto">
-                {/* <AddArgument /> */}
-                {/* <EditReturnValue /> */}
-                {/* <NoPermissionBox /> */}
-                {/* <PluginDetails /> */}
-                {/* <ChoosePaper /> */}
-                {/* <EditPaper /> */}
-                {/* <AddNewPaper /> */}
-                <ResetPaper />
-            </Alert>
-
-            {/* ---------------------------- */}
-            {/* <ToolTip /> */}
-            {/* <ToolTip tipContent="hello">FDFDS</ToolTip> */}
-            {/* ---------------------------- */}
-            {/* <HoverContent /> */}
-
-            {/* ---------------------------- */}
-            {/* <div style={{ border: '1px solid #ccc' }}>
-                <TextEditor id="a" handleAddScript={() => console.log('a')} />
-            </div> */}
-
-            {/* plugin drop down */}
-            {/* <PluginDropDown /> */}
-
-            {/* tab pane */}
-            {/* <Tabs defaultActiveKey="1">
-                <TabPane tabId="1" tab="My Plugins">
-                    My Plugins
-                </TabPane>
-                <TabPane tabId="2" tab="Plugins in Market">
-                    Plugins in Market
-                </TabPane>
-                <TabPane tabId="3" tab="Plugins in my Organization">
-                    Plugins in my Organization
-                </TabPane>
-            </Tabs> */}
-
-            {/* se */}
-            {/* <SearchPluginInput /> */}
-
-            <div style={{ width: '503px', height: '660px', border: '1px solid #ccc' }}>
-                {/* <PluginsCard /> */}
-                {/* <ABTestingCard /> */}
-            </div>
-        </div>
-    );
+    return <div style={{ marginTop: '200px', marginLeft: '200px' }}>hello</div>;
 };
 export default Home;
 /* <------------------------------------ **** FUNCTION COMPONENT END **** ------------------------------------ */
--- App/src/Route/index.tsx
+++ App/src/Route/index.tsx
@@ -4,9 +4,8 @@
  * author: Frank
  * lastModify: Frank 2020-07-21
  */
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import { Loading } from '@datareachable/dr_front_componentlibrary';
 /* <------------------------------------ **** Lazy Loading all the pages START **** ------------------------------------ */
 
 const Homepage = React.lazy(() => import(/* webpackChunkName: 'EditPage' */ '../Pages/HomePage'));
@@ -14,13 +13,11 @@ const Homepage = React.lazy(() => import(/* webpackChunkName: 'EditPage' */ '../
 
 const RootRouter = (): JSX.Element => {
     return (
-        <Suspense fallback={<Loading />}>
-            <Router>
-                <Switch>
-                    <Route path="/" component={Homepage} />
-                </Switch>
-            </Router>
-        </Suspense>
+        <Router>
+            <Switch>
+                <Route path="/" component={Homepage} />
+            </Switch>
+        </Router>
     );
 };
 
