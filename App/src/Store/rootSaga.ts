aaea97c7f0a3db89621cc7e67254632fb435b617
bin <bin.zhou@datareachable.com>
2022-03-10 14:34:27 +0800

    🎨 test 2022/03/10

--- App/src/Store/rootSaga.ts
+++ App/src/Store/rootSaga.ts
@@ -5,28 +5,11 @@
  * @lastModify Frank 2020-09-22
  */
 import { all } from 'redux-saga/effects';
-import loginSaga from './LoginState/sagas';
-import userSaga from './UserState/sagas';
-import organizationSaga from './OrganizationState/sagas';
-import talentGroupSaga from './TalentGroupState/sagas';
-import projectState from './ProjectState/sagas';
 // import homePageSaga from './HomePage/sagas';
-import categoryState from './CategoryState/sagas';
-import surveysSaga from './SurveyState/sagas';
 
 export default function* rootSaga(): Generator {
     try {
-        yield all([
-            // this is where the saga combine into the rootSaga
-            ...loginSaga,
-            ...userSaga,
-            ...organizationSaga,
-            ...projectState,
-            ...talentGroupSaga,
-            // ...homePageSaga,
-            ...categoryState,
-            ...surveysSaga,
-        ]);
+        yield all([]);
     } catch (err) {
         // This is where error monitoring should go
         console.log('error caught in rootsaga::', err);
